87b70da866e49a31631b0f407525f29333d7a443
Henry Lourtie <henry.lourtie@gmail.com>
2023-11-26 18:37:19 +0100

    finished project

--- ownGrep/grep.rs
+++ ownGrep/grep.rs
@@ -1,20 +1,12 @@
 use std::io::prelude::*;
 use std::env;
+use std::path::Path;
 use std::fs::File;
 use std::io::BufReader;
 
-fn get_file_content(file_name : &String) -> Result<String, std::io::Error> {
-
-    let file_to_read = File::open(&file_name)?;
-    let mut bufferreader = BufReader::new(file_to_read);
-    let mut file_contents = String::new();
-    bufferreader.read_to_string(&mut file_contents)?;
-    Ok(file_contents)
-} 
-
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //dbg!(args.clone());
+
     let argslen = args.clone().len();
     if argslen < 3 {
         println!("Lacking a file to search ");
@@ -26,11 +18,17 @@ fn main() {
         return;
     }
 
-    let file_contents = match get_file_content(&args[2]){
+    let file = match File::open(&Path::new(&args[2])){
         Err(why)=> panic!("couldn't open and read the file because {}", why),
         Ok(contents) => contents
     };
-    println!("{}",file_contents);
-
+    for (line_num, line) in BufReader::new(file).lines().enumerate(){
+        if let Ok(line) = line{
+            if line.contains(&args[1]){
+                println!("{} : {}", line_num+1, line);
+            }
+        }
+    }
     
+
 }
