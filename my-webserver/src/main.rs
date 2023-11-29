55b0dd848c1d0dc7563fd43cecfa425d37705efb
Henry Lourtie <henry.lourtie@gmail.com>
2023-11-29 21:48:41 +0100

    embeded migrations

--- my-webserver/src/main.rs
+++ my-webserver/src/main.rs
@@ -1,25 +1,5 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
-struct AppState {
-    app_name: String,
-}
-
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name; // <- get app_name
-    format!("URL {app_name}!") // <- response with app_name
-}
-
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().app_data(web::Data::new(AppState {
-            app_name: String::from("Shortener"),
-        }))
-        .service(index)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
-}
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
+mod db;
