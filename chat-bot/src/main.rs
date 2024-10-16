bca03a710bd9285aabd050083e89068eb37f8acd
Henry Lourtie <henry.lourtie@gmail.com>
2024-10-16 18:18:21 +0200

    bot functions

---
+++ chat-bot/src/main.rs
@@ -0,0 +1,109 @@
+
+use reqwest::Client;
+use reqwest_oauth1::OAuthClientProvider;
+use scraper::{Html, Selector};
+use dotenv::dotenv;
+use std::env;
+use serde::Serialize;
+use serde_json::json;
+use std::error::Error;
+use tokio;
+
+
+async fn post_tweet_v2(
+    consumer_key: &str,
+    consumer_secret: &str,
+    access_token: &str,
+    access_token_secret: &str,
+    tweet_text: &str,
+) -> Result<(), Box<dyn Error>> {
+
+    let url = "https://api.twitter.com/2/tweets";
+    println!("here2");
+    let tweet_text = if tweet_text.chars().count() > 280 {
+        &tweet_text[..280]
+    } else {
+        tweet_text
+    };
+    println!("{:?}", tweet_text);
+    let tweet = json!({
+        "text": tweet_text.to_string(),
+    });
+    let secrets = reqwest_oauth1::Secrets::new(consumer_key, consumer_secret).token(access_token, access_token_secret);
+
+// Create an HTTP client
+
+ let client = Client::new();
+ let body = serde_json::to_string(&tweet)?;
+// // Send the POST request with headers and JSON body
+// let response = client
+//     .post(url)
+//     .oauth1(secrets)
+//     .json(body)
+//     .send()
+//     .await?;
+// Build a request using reqwest
+
+let response = client.oauth1(secrets)
+.post(url)
+.header("Content-Type", "application/json") // Set the content type header to JSON
+        .body(body)
+.send().await?;
+
+    // Check if the request was successful
+    if response.status().is_success() {
+        println!("Tweet posted successfully!");
+        Ok(())
+    } else {
+        // If an error occurred, print the error response
+        let error_text = response.text().await?;
+        eprintln!("Failed to post tweet: {}", error_text);
+        Err("Failed to post the tweet".into())
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    dotenv().ok();
+    let body = reqwest::get("https://www.meteo.be/fr/ixelles").await?.text().await?;
+    let weather_body = Html::parse_document(&body);
+    let selector = Selector::parse("now-cast").unwrap();
+    let mut tweet_content =String::new(); 
+
+    for elem in weather_body.select(&selector){
+        let body_elems = elem.text().collect::<Vec<_>>();
+        tweet_content.push_str(body_elems[0]);
+        tweet_content.push_str(" ");
+    }
+    let key =env::var("DEEPL_AUTH_KEY")?;
+    let client = Client::new();
+    let data = json!({
+        "text":[tweet_content],
+        "target_lang":"EN"
+    });
+    let response = client.post("https://api-free.deepl.com/v2/translate")
+    .header("Authorization", format!("DeepL-Auth-Key {}", key))
+    .header("Content-Type", "application/json")
+    .json(&data)
+    .send().await?;
+
+    let english_text = if response.status().is_success() {
+        response.text().await?
+    } else {
+        eprintln!("Failed to translate. Status: {}", response.status());
+        String::new()
+    };
+    let parsed_json:serde_json::Value = serde_json::from_str(&english_text)?;
+    let english_tweet= parsed_json["translations"][0]["text"].as_str().unwrap_or("No weather today");
+    println!("english tweet: {:?}", english_tweet);
+    let consumer_key = env::var("CONSUMER_KEY")?;
+    let consumer_secret = env::var("CONSUMER_SECRET")?;
+    let access_token = env::var("OAUTH_TOKEN")?;
+    let access_token_secret = env::var("OAUTH_TOKEN_SECRET")?;
+    println!("here");
+    post_tweet_v2(&consumer_key, &consumer_secret, &access_token, &access_token_secret, english_tweet).await?;
+    
+   //println!("English {:?}", english_tweet);
+
+    Ok(())
+}
